f4f7863e285b276e7418a8554877b57dee43961a
aicha709 <aychakhazen3@gmail.com>
2022-05-11 15:21:48 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
+""" You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes. """
 
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    if len(boxes) == 0:
-        return False
-    keys = []
-    keys.append(0)
-    for key in keys:
-        new_keys = boxes[key]
-        for new_key in new_keys:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    """ You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes. """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
