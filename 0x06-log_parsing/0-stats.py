28d444ff1009a99e391d49cd5b80808541769b6a
aicha709 <aychakhazen3@gmail.com>
2022-06-09 21:45:26 +0100

    log-parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
-""" Script that reads stdin line by line and computes metrics."""
+"""
+Script that reads stdin line by line and computes metrics
+"""
+
 
 import sys
 
-dlist = {"size": 0,
-         "lines": 1}
 
-errors = {"200": 0, "301": 0, "400": 0, "401": 0,
-          "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
+                '405': 0, '500': 0}
+counter = 0
+size = 0
 
+try:
+    for l in sys.stdin:
+        data = l.split()
+        data = data[::-1]
 
-def printf():
-    """ Print codes and numbers"""
-    print("File size: {}".format(dlist["size"]))
-    for key in sorted(errors.keys()):
-        if errors[key] != 0:
-            print("{}: {}".format(key, errors[key]))
+        if len(data) > 2:
+            counter += 1
+            if counter <= 10:
+                size += int(data[0])
+                code = data[1]
 
+            if code in status_codes.keys():
+                status_codes[code] += 1
 
-def datasize(data):
-    """ Count file codes and size"""
-    dlist["size"] += int(data[-1])
-    if data[-2] in errors:
-        errors[data[-2]] += 1
-
-
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            try:
-                datasize(line.split(" "))
-            except:
-                pass
-            if dlist["lines"] % 10 == 0:
-                printf()
-            dlist["lines"] += 1
-    except KeyboardInterrupt:
-        printf()
-        raise
-    printf()
+            if counter == 10:
+                print('File size: {}'.format(size))
+                for sc, v in sorted(status_codes.items()):
+                    if v != 0:
+                        print('{}: {}'.format(sc, v))
+                counter = 0
+finally:
+    print('File size: {}'.format(size))
+    for sc, v in sorted(status_codes.items()):
+        if v != 0:
+            print('{}: {}'.format(sc, v))
